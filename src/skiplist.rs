226150e3f1e2ef48706dd0efd5847d22ef1b6a0a
Emil Hofstetter <emil.k.hofstetter@gmail.com>
2022-12-12 00:01:44 -0500

    Add SkipList trait

---
+++ src/skiplist.rs
@@ -0,0 +1,22 @@
+pub trait SkipList<K, V> {
+    type Entry<'a>
+    where
+        K: 'a,
+        V: 'a;
+
+    fn new() -> Self;
+
+    fn insert(&mut self, key: K, value: V);
+
+    fn insert_or_replace(&mut self, key: K, value: V) -> bool;
+
+    fn get<'a>(&self, key: &K) -> Option<Self::Entry<'a>>;
+
+    fn remove(&mut self, key: &K) -> Option<(K, V)>;
+
+    fn front<'a>(&self) -> Option<Self::Entry<'a>>;
+
+    fn last<'a>(&self) -> Option<Self::Entry<'a>>;
+
+    fn len(&self) -> usize;
+}
