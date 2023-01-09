a40fe55b1a92bf7fd3827012e0082ead1150f0ca
Emil Hofstetter <emil.k.hofstetter@gmail.com>
2023-01-08 23:53:21 -0500

    Fix bug in link_node that caused timeout loop and improve fuzzing

--- fuzz/fuzz_targets/fuzz_target_1.rs
+++ fuzz/fuzz_targets/fuzz_target_1.rs
@@ -1,27 +1,40 @@
 #![no_main]
 
+use afl;
 use libfuzzer_sys::fuzz_target;
 use rand::Rng;
 use skippy_rs::SyncSkipList;
 use std::sync::Arc;
 
-fuzz_target!(|data: &[u8]| {
+const fn randomize(mut seed: usize) -> usize {
+    seed ^= seed << 13;
+    seed ^= seed >> 17;
+    seed ^ seed << 5
+}
+
+fuzz_target!(|seeds: Vec<usize>| {
     let list = Arc::new(SyncSkipList::new());
 
-    let threads = (0..20)
-        .map(|_| {
+    let threads = seeds
+        .into_iter()
+        .map(|mut seed| {
             let list = list.clone();
-            std::thread::spawn(move || {
-                let mut rng = rand::thread_rng();
-                for _ in 0..5_000 {
-                    let target = rng.gen::<u8>();
-                    if rng.gen::<u8>() % 5 == 0 {
-                        list.remove(&target);
-                    } else {
-                        list.insert(target, ());
+            unsafe {
+                std::thread::spawn(move || {
+                    let mut seed = seed;
+                    for _ in 0..5_000 {
+                        seed = randomize(seed);
+
+                        if seed % 5 == 0 {
+                            seed = randomize(seed);
+                            list.remove(&(*(&seed as *const usize as *const u8)));
+                        } else {
+                            seed = randomize(seed);
+                            list.insert(*(&seed as *const usize as *const u8), ());
+                        }
                     }
-                }
-            })
+                })
+            }
         })
         .collect::<Vec<_>>();
 
